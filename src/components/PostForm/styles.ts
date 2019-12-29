db62b28fdea2f444ce067485f4252ec3a07dd212
unknown <denis.kustura@fer.hr>
2019-12-29 18:23:04 +0100

    Add form for creating a new post

---
+++ src/components/PostForm/styles.ts
@@ -0,0 +1,22 @@
+import { createStyles, Theme } from '@material-ui/core';
+
+export default (theme: Theme) =>
+  createStyles({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.primary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(3),
+    },
+    submit: {
+      margin: theme.spacing(0, 0, 2),
+    },
+  });
