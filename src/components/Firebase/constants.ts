6da6ba55bc5e413c25bb16506bccd37d9f3fcc1e
unknown <denis_kustura@hotmail.com>
2020-01-11 23:15:42 +0100

    Add Post cloud function methods

--- src/components/Firebase/constants.ts
+++ src/components/Firebase/constants.ts
@@ -6,6 +6,10 @@ export const ADD_MEMBER_ROLE_FUNCTION = 'addMemberRole';
 export const REMOVE_MEMBER_ROLE_FUNCTION = 'removeMemberRole';
 export const SET_MEMBER_ROLE_FUNCTION = 'setMemberRole';
 
+export const CREATE_POST_FUNCTION = 'createPost';
+export const UPDATE_POST_FUNCTION = 'updatePost';
+export const DELETE_POST_FUNCTION = 'deletePost';
+
 export const functionNames = {
   ADD_ADMIN_ROLE_FUNCTION,
   REMOVE_ADMIN_ROLE_FUNCTION,
@@ -13,4 +17,7 @@ export const functionNames = {
   ADD_MEMBER_ROLE_FUNCTION,
   REMOVE_MEMBER_ROLE_FUNCTION,
   SET_MEMBER_ROLE_FUNCTION,
+  CREATE_POST_FUNCTION,
+  UPDATE_POST_FUNCTION,
+  DELETE_POST_FUNCTION,
 };
