1ded7799434eeb483f7e9f360ef830e68e9cd73b
unknown <denis_kustura@hotmail.com>
2020-02-26 18:35:11 +0100

    Fix smoke test

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -2,8 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './index';
 
+import { Provider } from 'react-redux';
+import store from '../../store';
+
+import { HashRouter } from 'react-router-dom';
+import Firebase, { FirebaseProvider } from 'components/Firebase';
+
+const firebase = new Firebase();
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <HashRouter>
+        <FirebaseProvider value={firebase}>
+          <App />
+        </FirebaseProvider>
+      </HashRouter>
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
