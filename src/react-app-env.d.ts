3c3037ca6f7bd43152fa7b7d28e54b2210944ee0
unknown <denis_kustura@hotmail.com>
2020-11-16 01:03:42 +0100

    :sparkles: Add image of the team to About us page and test of hover stroke

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -3,4 +3,6 @@
 // This is needed in order to import images as modules:
 // import imageUrl from './myImage.jpg';
 declare module '*.jpg';
+declare module '*.jpeg';
 declare module '*.png';
+declare module '*.svg';
